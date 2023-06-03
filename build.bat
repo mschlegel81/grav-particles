1d5879c8135838f715db85224f267ffe91cf471a
Martin Schlegel <schlegel@itestra.com>
2023-06-03 15:52:20 +0200

    Fixed build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 ..\lazarus64\lazbuild.exe gravityMain.lpi --bm=default
 ..\lazarus64\fpc\3.2.2\bin\x86_64-win64\delp.exe -r .
 upx.exe --best --ultra-brute -o gravity_upx.exe gravity.exe
+move /Y gravity_upx.exe gravity.exe
